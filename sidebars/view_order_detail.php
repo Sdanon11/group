98c540daa691bb1f6706049e6c335f2b1c13573d
Sdanon11 <65010912563@msu.ac.th>
2024-10-20 19:08:54 +0700

    กกก

--- sidebars/view_order_detail.php
+++ sidebars/view_order_detail.php
@@ -75,7 +75,7 @@
 <body>
     <div class="container">
         <h1>รายละเอียดใบสั่งซื้อ เลขที่ <?= htmlspecialchars($_GET['a']); ?></h1>
-        <a href="view_order.php" class="btn btn-secondary" style="float: left;">กลับไปหน้ารายการสินค้า</a>
+        <a href="view_order.php" class="btn btn-secondary" style="float: left;">กลับไปหน้ารายการสั่งซื้อ</a>
         <table>
             <tr>
                 <th>ที่</th>
